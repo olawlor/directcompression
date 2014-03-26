511293b8182d25038c9b93b13a5e0ca5a2c5979d
Dr. Orion Lawlor <lawlor@alaska.edu>
2014-03-26 01:19:46 -0800

    More general screen dump function.

--- include/ogl/dumpscreen.h
+++ include/ogl/dumpscreen.h
@@ -3,17 +3,45 @@
 #include <vector>
 #include <fstream>
 
-inline void oglDumpScreen(void) {
+inline std::vector<unsigned char> oglDumpScreen(int &w,int &h,int numComponents=3) {
+	GLenum glComponents=GL_RGB;
+	switch (numComponents) {
+	case 1: glComponents=GL_ALPHA; break;
+	case 3: default: numComponents=3; glComponents=GL_RGB; break;
+	case 4: glComponents=GL_RGBA; break;
+	}
+	
 	int dims[4]; glGetIntegerv(GL_VIEWPORT,dims);
-	int w=dims[2], h=dims[3];
-	static std::vector<unsigned char> rgb; /* static avoids repeated allocation */
-	rgb.resize(w*h*3);
+	w=dims[2], h=dims[3];
+	std::vector<unsigned char> data;
+	data.resize(w*h*numComponents);
 	glPixelStorei(GL_PACK_ALIGNMENT,1); /* no particular alignment */
-	char name[100]; 
-	glReadPixels(dims[0],dims[1],w,h, GL_RGB,GL_UNSIGNED_BYTE,&rgb[0]);
+	glReadPixels(dims[0],dims[1],w,h, 
+		glComponents,
+		GL_UNSIGNED_BYTE,&data[0]);
+	return data;
+}
+
+inline void oglDumpScreen(void) {
+	int w,h;
+	static std::vector<unsigned char> data; /* static avoids repeated allocation */
+	data=oglDumpScreen(w,h,3);
 	static int imageCount=0;
+	char name[100]; 
 	sprintf(name,"image%05d.ppm",imageCount++);
 	std::ofstream of(name,std::ios_base::binary);
 	of<<"P6\n"<<w<<" "<<h<<"\n255\n";
-	of.write((char *)&rgb[0],w*h*3);
+	of.write((char *)&data[0],w*h*3);
+}
+
+inline void oglDumpAlpha(void) {
+	int w,h;
+	static std::vector<unsigned char> data; /* static avoids repeated allocation */
+	data=oglDumpScreen(w,h,1);
+	char name[100]; 
+	static int imageCount=0;
+	sprintf(name,"image%05d_alpha.ppm",imageCount++);
+	std::ofstream of(name,std::ios_base::binary);
+	of<<"P5\n"<<w<<" "<<h<<"\n255\n";
+	of.write((char *)&data[0],w*h);
 }
